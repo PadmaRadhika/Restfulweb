8f2dcb2e9635e4ceeb10ca822aaf7e722ec9e040
PadmaRadhika <radhika.piratla@gmail.com>
2024-11-14 16:46:18 -0800

    added restful web files

---
+++ src/main/java/com/example/restfulweb/GoodByeService.java
@@ -0,0 +1,10 @@
+package com.example.restfulweb;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class GoodByeService {
+	public  String goodBye() {
+		return "Thats all folks";
+	}
+}
